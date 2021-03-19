d619f2b933ac126e3f92f4e289a7db48e3f63d4f
JefersonEGomes <jefersonsportsempre@gmail.com>
2021-03-19 16:37:27 -0300

    adicionando códigos de exemplo

--- FUNCTIONS/functions.js
+++ FUNCTIONS/functions.js
@@ -1,6 +1,43 @@
-// Manipulando Strings e Números
+// Operadores de comparação
 
-// Transformar uma cadeia de caracteres em elementos de um array
+// Irá comprar valores e retornar um Boolean como resposta á comparação
 
-let word = "manipulação"
-console.log(Array.from(word))
+let one = 1
+let two = 2
+
+// ==   Igual
+// console.log(one == "1") // true
+// console.log(one == 1) // true
+// console.log(one == two) // false
+
+// !=   Diferente
+// console.log(one != "1") // false
+// console.log(one != 1) // false
+// console.log(one != two) // true
+
+// ===  Estritamente igual
+// console.log(one === "1") // false
+// console.log(one === two) // false
+// console.log(one === 1) // true
+
+// !==  Estritamente diferente
+// console.log(one !== two) // true
+// console.log(one !== "1") // true
+// console.log(one !== 1) // false
+
+// >    Maior
+// console.log(one > two) // false
+
+// >=   Maior igual
+// console.log(one >= 1) // true
+// console.log(two >= 1) // true
+
+// <    Menor
+// console.log(one < two) // true
+
+// <=   Menor igual
+// console.log(one <= 1) // true
+// console.log(one <= two) // true
+// console.log(one <= 0) // false
+
+//
